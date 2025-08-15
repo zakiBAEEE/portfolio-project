5e39a5de625151cec9a117212c429b9f68f41af9
Muhammad Dzaky Raihan <muhammaddzakyraihan_2226250100@mhs.mdp.ac.id>
2025-08-15 21:26:20 +0700

    feat (ui) : Menambah animasi pada tab

--- frontend/portfolio-frontend/src/App.jsx
+++ frontend/portfolio-frontend/src/App.jsx
@@ -19,11 +19,11 @@ function App() {
       <div className="relative z-10 p-8 text-white  justify-center w-[50%]">
         <div className="flex flex-row justify-start gap-7 text-white mb-5">
           <p className={`font-semibold text-3xl ${activeTabs == 'me' ?
-            'bg-emerald-700' : 'bg-transparent'} cursor-pointer rounded-lg p-2`} onClick={() => handleTabs("me")}>
+            'bg-sky-700 min-w-[25rem] opacity-90' : 'bg-transparent min-w-[8rem]'} cursor-pointer rounded-lg p-2 transition-all duration-800`} onClick={() => handleTabs("me")}>
             Muhammad Dzaky R
           </p>
           <p className={`font-semibold text-3xl ${activeTabs == 'portfolio' ?
-            'bg-emerald-700' : 'bg-transparent'} cursor-pointer rounded-lg p-2`} onClick={() => handleTabs("portfolio")}>
+            'bg-sky-700 min-w-[15rem] opacity-90' : 'bg-transparent min-w-[8rem]'} cursor-pointer rounded-lg p-2 transition-all duration-800`} onClick={() => handleTabs("portfolio")}>
             Portfolio
           </p>
         </div>
