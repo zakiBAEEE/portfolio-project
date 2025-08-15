5b0aa4d151cd683d9a8b03cbd8897eda23eaaf52
Muhammad Dzaky Raihan <muhammaddzakyraihan_2226250100@mhs.mdp.ac.id>
2025-08-15 21:52:06 +0700

    feat (ui) : Implementasi Localstorage

--- frontend/portfolio-frontend/src/App.jsx
+++ frontend/portfolio-frontend/src/App.jsx
@@ -1,10 +1,14 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Me from "./components/me";
 
 
 function App() {
 
-  const [activeTabs, setActiveTabs] = useState("me");
+  const [activeTabs, setActiveTabs] = useState(
+    localStorage.getItem('activeTabs') || 'me'
+  );
+
+  useEffect(() => localStorage.setItem('activeTabs', activeTabs), [activeTabs])
 
   const handleTabs = (tab) => {
     setActiveTabs(tab);
@@ -16,14 +20,14 @@ function App() {
       <div className="absolute inset-0 bg-[url('/bekron.jpg')] bg-cover bg-center blur-xl scale-105"></div>
 
       {/* Content layer */}
-      <div className="relative z-10 p-8 text-white  justify-center w-[50%]">
+      <div className="relative z-10 p-8 text-white  justify-center w-[50%] mt-14">
         <div className="flex flex-row justify-start gap-7 text-white mb-5">
           <p className={`font-semibold text-3xl ${activeTabs == 'me' ?
-            'bg-sky-700 min-w-[25rem] opacity-90' : 'bg-transparent min-w-[8rem]'} cursor-pointer rounded-lg p-2 transition-all duration-800`} onClick={() => handleTabs("me")}>
+            'bg-emerald-300/40 min-w-[25rem] opacity-90' : 'bg-transparent min-w-[8rem]'} cursor-pointer rounded-lg p-2 transition-all duration-800 ease-in-out`} onClick={() => handleTabs("me")}>
             Muhammad Dzaky R
           </p>
           <p className={`font-semibold text-3xl ${activeTabs == 'portfolio' ?
-            'bg-sky-700 min-w-[15rem] opacity-90' : 'bg-transparent min-w-[8rem]'} cursor-pointer rounded-lg p-2 transition-all duration-800`} onClick={() => handleTabs("portfolio")}>
+            'bg-emerald-300/40 min-w-[15rem] opacity-90' : 'bg-transparent min-w-[8rem]'} cursor-pointer rounded-lg p-2 transition-all duration-800 ease-in-out`} onClick={() => handleTabs("portfolio")}>
             Portfolio
           </p>
         </div>
